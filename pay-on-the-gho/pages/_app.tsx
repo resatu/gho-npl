d730c8412629117de210b47e87aba980a7456c16
resatugurulu <resatugur.ulu@gmail.com>
2024-01-17 10:30:02 +0300

    add wip connectkit

--- pay-on-the-gho/pages/_app.tsx
+++ pay-on-the-gho/pages/_app.tsx
@@ -2,15 +2,31 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from '@/components/Layout'; // Assuming you have a Layout component
+import { ConnectKitProvider } from 'connectkit';
+import { WagmiConfig } from 'wagmi';
 
 const queryClient = new QueryClient();
+
+const config = createConfig(
+  getDefaultConfig({
+    appName: 'ConnectKit Next.js demo',
+    //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
+    //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    chains: [mainnet, polygon, optimism, arbitrum],
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+  })
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout title={''} description={''}>
-        <Component {...pageProps} />
-      </Layout>
+      <WagmiConfig config={config}>
+        <ConnectKitProvider debugMode>
+          <Layout title={''} description={''}>
+            <Component {...pageProps} />
+          </Layout>
+        </ConnectKitProvider >
+      </WagmiConfig>
     </QueryClientProvider>
   );
 }
